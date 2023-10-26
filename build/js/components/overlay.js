14d4e0301648dbf346f300737271d09226b725b6
dimasardnt6 <dimasardnt6@gmail.com>
2023-10-26 17:47:30 +0700

    update fungsi sidebar

--- build/js/components/overlay.js
+++ build/js/components/overlay.js
@@ -1,13 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
+const setupSidebar = () => {
   const sidebarToggleButton = document.getElementById("sidebar-toggle-button");
   const sidebar = document.getElementById("application-sidebar");
-  const closeSidebarButton = document.getElementById("close-sidebar-button");
 
-  sidebarToggleButton.addEventListener("click", function () {
+  const toggleSidebar = (event) => {
+    event.stopPropagation(); // Menghentikan event klik dari menyebar ke dokumen
     sidebar.classList.toggle("hidden");
-  });
+  };
 
-  closeSidebarButton.addEventListener("click", function () {
-    sidebar.classList.toggle("hidden");
+  const closeSidebar = (event) => {
+    const { target } = event;
+    // Periksa apakah yang diklik bukanlah bagian dari sidebar atau tombol toggle
+    if (target !== sidebar) {
+      // Sembunyikan sidebar jika tidak diklik di dalam sidebar atau tombol toggle
+      sidebar.classList.add("hidden");
+    }
+  };
+
+  // Event listener untuk mencegah event klik di dalam sidebar dari menyebar ke dokumen
+  sidebarToggleButton.addEventListener("click", toggleSidebar);
+  document.addEventListener("click", closeSidebar);
+  sidebar.addEventListener("click", (event) => {
+    event.stopPropagation();
   });
-});
+};
+
+document.addEventListener("DOMContentLoaded", setupSidebar);
