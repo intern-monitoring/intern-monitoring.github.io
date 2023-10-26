0dd4d634e7e4ebc5cbc64c03920e1c6077a7f115
dimasardnt6 <dimasardnt6@gmail.com>
2023-10-26 16:42:23 +0700

    delete file

--- build/js/index.js
+++
@@ -1,3 +0,0 @@
-import PostSignIn from "https://cdn.jsdelivr.net/gh/intern-monitoring/jsintermoni@main/post/postSignIn.js";
-
-window.PostSignIn = PostSignIn;
--- build/js/post.js
+++
@@ -1,5 +0,0 @@
-// import { postWithToken } from "https://jscroot.github.io/api/croot.js";
-// import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
-import PostSignUp from "https://cdn.jsdelivr.net/gh/dimasardnt6/testing.github.io@main/postFunction.js";
-
-window.PostSignUp = PostSignUp;
