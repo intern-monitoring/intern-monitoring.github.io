cdfd5af37030eb49fe9d86510627efe5e1c917d2
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-02 16:40:33 +0700

    update delete function

--- pages/mitra/assets/js/delete.js
+++ pages/mitra/assets/js/delete.js
@@ -1,39 +1,51 @@
-const deleteMagang = (IDHAPUS) => {
-  try {
-    const magangId = IDHAPUS;
-    const target_url =
-      "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-delete-magang?id=" +
-      magangId;
+document.addEventListener("DOMContentLoaded", function () {
+  const deleteMagang = (IDHAPUS) => {
+    try {
+      const magangId = IDHAPUS;
+      const target_url =
+        "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-delete-magang?id=" +
+        magangId;
 
-    const requestOptions = {
-      method: "DELETE",
-      redirect: "follow",
-    };
+      const requestOptions = {
+        method: "DELETE",
+        redirect: "follow",
+      };
 
-    const response = fetch(target_url, requestOptions);
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
+      fetch(target_url, requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          Swal.fire({
+            icon: "success",
+            title: "Data berhasil dihapus",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          location.reload();
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          Swal.fire({
+            icon: "error",
+            title: "Gagal menghapus data",
+            text: "Terjadi kesalahan saat menghapus data. Silakan coba lagi.",
+            showConfirmButton: true,
+            confirmButtonText: "OK",
+          });
+        });
+    } catch (error) {
+      console.error("Error:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Gagal menghapus data",
+        text: "Terjadi kesalahan saat menghapus data. Silakan coba lagi.",
+        showConfirmButton: true,
+        confirmButtonText: "OK",
+      });
     }
-
-    Swal.fire({
-      icon: "success",
-      title: "Data berhasil dihapus",
-      showConfirmButton: false,
-      timer: 1500,
-    });
-
-    location.reload();
-  } catch (error) {
-    console.error("Error:", error);
-    Swal.fire({
-      icon: "error",
-      title: "Gagal menghapus data",
-      text: "Terjadi kesalahan saat menghapus data. Silakan coba lagi.",
-      showConfirmButton: true,
-      confirmButtonText: "OK",
-    });
-  }
-};
-
-window.deleteMagang = deleteMagang;
+  };
+});
