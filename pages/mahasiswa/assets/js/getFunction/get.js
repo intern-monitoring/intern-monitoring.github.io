b355ef5451935b6c467dd33dec83fea567b7dc7f
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-03 20:36:32 +0700

    update get.js

--- pages/mahasiswa/assets/js/getFunction/get.js
+++ pages/mahasiswa/assets/js/getFunction/get.js
@@ -1,12 +1,8 @@
 import { URLGetMagang, responseData } from "./getMagang.js";
-import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 const get = (target_url, responseFunction) => {
-  const myHeaders = new Headers();
-  myHeaders.append("Authorization", getCookie("Authorization"));
   const requestOptions = {
     method: "GET",
-    headers: myHeaders,
     redirect: "follow",
   };
 
