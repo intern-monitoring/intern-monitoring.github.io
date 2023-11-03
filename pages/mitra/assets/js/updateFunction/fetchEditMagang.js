fa67caaf9df14f8da73e90f2094aa7a47670cc06
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-03 12:00:57 +0700

    push

--- pages/mitra/assets/js/updateFunction/fetchEditMagang.js
+++ pages/mitra/assets/js/updateFunction/fetchEditMagang.js
@@ -1,11 +1,5 @@
 import { get } from "https://jscroot.github.io/api/croot.js";
 import { isiData } from "./editMagang.js";
-
-const urlParams = new URLSearchParams(window.location.search);
-const magangId = urlParams.get("magangId");
-
-let urlFetch =
-  "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-magang-mitra/" +
-  magangId;
+import { urlFetch } from "./urlEdit.js";
 
 get(urlFetch, isiData);
