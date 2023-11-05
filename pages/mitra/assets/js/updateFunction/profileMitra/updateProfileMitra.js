b20e1be231e3804b109052109cbe0199f553b97a
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-05 23:59:51 +0700

    Fix import statement in updateProfileMitra.js

--- pages/mitra/assets/js/updateFunction/profileMitra/updateProfileMitra.js
+++ pages/mitra/assets/js/updateFunction/profileMitra/updateProfileMitra.js
@@ -1,6 +1,6 @@
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
-import { urlPUT, AmbilResponse } from "./urlPutProfileMitra";
+import { urlPUT, AmbilResponse } from "./urlPutProfileMitra.js";
 
 console.log("hadeer");
 
