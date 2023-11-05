e726526d36d8937585bae0a30b9986c4961e28b7
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-05 23:50:24 +0700

    Add new files and update existing ones for URL editing and fetching

--- pages/mitra/assets/js/getFunction/profileMitra/getProfileMitra.js
+++ pages/mitra/assets/js/getFunction/profileMitra/getProfileMitra.js
@@ -1,9 +1,9 @@
 import { addInner } from "https://jscroot.github.io/element/croot.js";
 
-export const URLGetProfileMahasiswa =
-  "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-mahasiswa";
+export const URLGetProfileMitra =
+  "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-mitra";
 
-export const dataMahasiswa = `
+export const dataProfileMitra = `
 <div class="bg-white rounded-xl shadow-md">
 <div
   class="relative h-48 rounded-t-xl bg-[url('https://images.unsplash.com/photo-1655635643532-fa9ba2648cbe?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2232&amp;q=80')] bg-no-repeat bg-cover bg-center"
@@ -13,7 +13,7 @@ export const dataMahasiswa = `
   ></span>
   <div class="absolute top-0 right-0 p-4">
     <a
-      href="editProfileMahasiswa?mahasiswaId=#IDEDIT#"
+      href="editProfileMitra?mitraId=#IDEDIT#"
       class="py-2 px-3 inline-flex cursor-pointer justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm"
     >
       <svg
@@ -38,6 +38,7 @@ export const dataMahasiswa = `
 </div>
 
 <div class="pt-0 p-4 sm:pt-0 sm:p-7">
+  <!-- profile -->
   <div>
     <div>
       <label class="sr-only"> Photo </label>
@@ -45,46 +46,72 @@ export const dataMahasiswa = `
       <div class="flex items-center justify-center gap-x-5">
         <img
           class="-mt-14 relative z-10 inline-block h-32 w-32 mx-auto sm:mx-0 rounded-full ring-4 ring-white"
-          src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+          src="../../images/netflix_logo.jpg"
           alt="Image Description"
         />
       </div>
     </div>
     <div class="flex justify-center gap-5 pt-5">
       <h2 class="text-2xl text-gray-900 font-semibold">
-        #NAMALENGKAP#
+        #NAMAPERUSAHAAN#
       </h2>
     </div>
-    <div class="flex justify-center gap-2">
-      <h3 class="text-base text-gray-900 font-normal">#NIM#</h3>
-    </div>
-    <div class="flex justify-center gap-2 pt-5">
+
+    <div class="flex justify-center gap-2 pt-10">
       <label class="text-base text-gray-900 font-semibold"
-        >Tanggal Lahir :</label
+        >Narahubung :</label
       >
       <h3 class="text-base text-gray-900 font-normal">
-        #TANGGALLAHIR#
+        NAMANARAHUBUNG
       </h3>
     </div>
     <div class="flex justify-center gap-2">
       <label class="text-base text-gray-900 font-semibold"
-        >Jenis Kelamin :</label
+        >Nomor Narahubung :</label
       >
-      <h3 class="text-base text-gray-900 font-normal">#GENDER#</h3>
+      <h3 class="text-base text-gray-900 font-normal">
+        #NOHP#
+      </h3>
+    </div>
+    <div class="flex justify-center gap-2">
+      <label class="text-base text-gray-900 font-semibold"
+        >Website Perusahaan :</label
+      >
+      <h3 class="text-base text-gray-900 font-normal">
+        #WEBSITE#
+      </h3>
     </div>
   </div>
   <div class="mt-2 border-b border-gray-200"></div>
+  <!-- End profile -->
 
-  <div class="grid grid-cols-2 py-5">
+  <!-- Perusahaan Section -->
+  <div class="grid gap-2 pt-10">
+    <div class="px-10 lg:px-20">
+      <div class="flex justify-center mb-2">
+        <p
+          class="inline-flex justify-center items-center gap-x-3 text-base text-center font-semibold text-gray-900 lg:text-lg"
+        >
+          Tentang Perusahaan
+        </p>
+      </div>
+      <p
+        class="flex justify-center text-xs text-center text-gray-500 lg:text-base"
+      >
+        #TENTANG#
+      </p>
+    </div>
+  </div>
+  <div class="grid grid-cols-3 gap-2 pt-20 pb-5">
     <div class="mx-auto">
       <div class="flex justify-center mb-2">
         <p
-          class="inline-flex justify-center items-center gap-x-3 text-xl text-center font-semibold text-gray-900"
+          class="inline-flex justify-center items-center gap-x-3 text-base text-center font-semibold text-gray-900 lg:text-lg"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
+            width="16"
+            height="16"
             fill="currentColor"
             class="bi bi-bank"
             viewBox="0 0 16 16"
@@ -93,20 +120,48 @@ export const dataMahasiswa = `
               d="m8 0 6.61 3h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.38l.5 2a.498.498 0 0 1-.485.62H.5a.498.498 0 0 1-.485-.62l.5-2A.501.501 0 0 1 1 13V6H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 3h.89L8 0ZM3.777 3h8.447L8 1 3.777 3ZM2 6v7h1V6H2Zm2 0v7h2.5V6H4Zm3.5 0v7h1V6h-1Zm2 0v7H12V6H9.5ZM13 6v7h1V6h-1Zm2-1V4H1v1h14Zm-.39 9H1.39l-.25 1h13.72l-.25-1Z"
             />
           </svg>
-          Perguruan Tinggi
+          Kategori
         </p>
       </div>
-      <p class="text-center text-base text-gray-500">#PERGURUAN#</p>
+      <p
+        class="flex justify-center text-xs text-center text-gray-500 lg:text-base"
+      >
+        #KATEGORI#
+      </p>
     </div>
     <div class="mx-auto">
       <div class="flex justify-center mb-2">
         <p
-          class="inline-flex justify-center items-center gap-x-3 text-xl text-center font-semibold text-gray-900"
+          class="inline-flex justify-center items-center gap-x-3 text-base text-center font-semibold text-gray-900 lg:text-lg"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-bank"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="m8 0 6.61 3h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.38l.5 2a.498.498 0 0 1-.485.62H.5a.498.498 0 0 1-.485-.62l.5-2A.501.501 0 0 1 1 13V6H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 3h.89L8 0ZM3.777 3h8.447L8 1 3.777 3ZM2 6v7h1V6H2Zm2 0v7h2.5V6H4Zm3.5 0v7h1V6h-1Zm2 0v7H12V6H9.5ZM13 6v7h1V6h-1Zm2-1V4H1v1h14Zm-.39 9H1.39l-.25 1h13.72l-.25-1Z"
+            />
+          </svg>
+          Alamat
+        </p>
+      </div>
+      <p class="text-center text-xs text-gray-500 lg:text-base">
+        #ALAMAT#
+      </p>
+    </div>
+    <div class="mx-auto">
+      <div class="flex justify-center mb-2">
+        <p
+          class="inline-flex justify-center items-center gap-x-3 text-base text-center font-semibold text-gray-900 lg:text-lg"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
             fill="currentColor"
             class="bi bi-stack"
             viewBox="0 0 16 16"
@@ -118,29 +173,36 @@ export const dataMahasiswa = `
               d="m14.12 6.576 1.715.858c.22.11.22.424 0 .534l-7.568 3.784a.598.598 0 0 1-.534 0L.165 7.968a.299.299 0 0 1 0-.534l1.716-.858 5.317 2.659c.505.252 1.1.252 1.604 0l5.317-2.659z"
             />
           </svg>
-          Prodi
+          Sektor
         </p>
       </div>
-      <p class="text-center text-base text-gray-500">#PRODI#</p>
+      <p
+        class="justify-center text-xs text-center text-gray-500 lg:text-base"
+      >
+        #SEKTOR#
+      </p>
     </div>
   </div>
+  <!-- End Perusahaan Section -->
 </div>
 </div>
 `;
 
-export function responseDataProfile(results) {
+export function responseData(results) {
   console.log(results);
-  isiRow(results);
+  results.forEach(isiRow);
 }
 
 export function isiRow(value) {
-  const content = dataMahasiswa
-    .replace("#NAMALENGKAP#", value.namalengkap)
-    .replace("#TANGGALLAHIR#", value.tanggallahir)
-    .replace("#GENDER#", value.jeniskelamin)
-    .replace("#NIM#", value.nim)
-    .replace("#PERGURUAN#", value.perguruantinggi)
-    .replace("#PRODI#", value.prodi)
+  const content = dataProfileMitra
+    .replace("#NAMANARAHUBUNG#", value.namanarahubung)
+    .replace("#NOHP#", value.nohpnarahubung)
+    .replace("#NAMAPERUSAHAAN#", value.nama)
+    .replace("#KATEGORI#", value.kategori)
+    .replace("#SEKTOR#", value.sektorindustri)
+    .replace("#TENTANG#", value.tentang)
+    .replace("#ALAMAT#", value.alamat)
+    .replace("#WEBSITE#", value.website)
     .replace("#IDEDIT#", value._id);
-  addInner("profileMahasiswa", content);
+  addInner("profileMitra", content);
 }
