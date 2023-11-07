2b2e6587b97b6a2a8d1ad142edd2d366f0665b3a
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-07 20:54:42 +0700

    Update background color of pages to gray-50

---
+++ pages/mitra/assets/js/getFunction/accountMitra/getAccountMitra.js
@@ -0,0 +1,19 @@
+import { URLGetAccountMitra, responseData } from "./getAccountMitraFunction.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+const get = (target_url, responseFunction) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", getCookie("Authorization"));
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => responseFunction(JSON.parse(result)))
+    .catch((error) => console.log("error", error));
+};
+
+get(URLGetAccountMitra, responseData);
