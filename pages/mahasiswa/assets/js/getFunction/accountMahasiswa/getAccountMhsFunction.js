9cb1a00ee8cdd3bc394437142d9d6a0bfa4b3287
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-09 18:29:36 +0700

    Fix import path for getAccountMhs.js

--- pages/mahasiswa/assets/js/getFunction/accountMahasiswa/getAccountMhsFunction.js
+++ pages/mahasiswa/assets/js/getFunction/accountMahasiswa/getAccountMhsFunction.js
@@ -1,4 +1,4 @@
-import { URLGetAccountMahasiswa, responseData } from "./getAccountMhs";
+import { URLGetAccountMahasiswa, responseData } from "./getAccountMhs.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 const get = (target_url, responseFunction) => {
