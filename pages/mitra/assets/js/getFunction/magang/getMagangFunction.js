b1bfcb58bb9c06e9ecfa66edf08ff5c84c3506e5
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-11 21:56:51 +0700

    Add count of results to magang page

--- pages/mitra/assets/js/getFunction/magang/getMagangFunction.js
+++ pages/mitra/assets/js/getFunction/magang/getMagangFunction.js
@@ -1,6 +1,14 @@
 import { URLGetMagang, responseData } from "./getMagang.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
+const CountMagang = (count) => {
+  const resultCountElement = document.getElementById("magangCount");
+  resultCountElement.innerHTML = `
+    <p class="text-sm text-gray-600">
+      <span class="font-semibold text-gray-800">${count}</span> results
+    </p>`;
+};
+
 const get = (target_url, responseFunction) => {
   const myHeaders = new Headers();
   myHeaders.append("Authorization", getCookie("Authorization"));
@@ -13,6 +21,7 @@ const get = (target_url, responseFunction) => {
   fetch(target_url, requestOptions)
     .then((response) => response.text())
     .then((result) => responseFunction(JSON.parse(result)))
+    .then((result) => CountMagang(result.length))
     .catch((error) => console.log("error", error));
 };
 
