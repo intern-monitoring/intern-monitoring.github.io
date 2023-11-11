158c540c63023d5032988cb55a46dd0ee1cbd4e9
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-11 14:50:17 +0700

    Add fade-in animation to elements

--- pages/js/animation/animate.js
+++ pages/js/animation/animate.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const fadeElementsLeft = document.querySelectorAll(".fade-in-element-left");
   const fadeElementsRight = document.querySelectorAll(".fade-in-element-right");
+  const fadeElementsBottom = document.querySelectorAll(
+    ".fade-in-element-bottom"
+  );
 
   function checkFade(elements) {
     elements.forEach(function (element) {
@@ -18,10 +21,12 @@ document.addEventListener("DOMContentLoaded", function () {
   // Initial check when the page loads
   checkFade(fadeElementsLeft);
   checkFade(fadeElementsRight);
+  checkFade(fadeElementsBottom);
 
   // Add a scroll event listener to check on scroll
   window.addEventListener("scroll", function () {
     checkFade(fadeElementsLeft);
     checkFade(fadeElementsRight);
+    checkFade(fadeElementsBottom);
   });
 });
