b5d5f438515770e637d5997b3057b4cf31d1cf46
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-13 15:48:04 +0700

    Add getSeleksiFunction.js and update icons in admin pages

---
+++ pages/admin/assets/js/getFunction/seleksiMagang/getSeleksiFunction.js
@@ -0,0 +1,34 @@
+import { URLGetSeleksi, responseData } from "./getSeleksiMagang.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+const CountSeleksi = (count) => {
+  const resultCountElement = document.getElementById("seleksiCount");
+  resultCountElement.innerHTML = `
+    <p class="text-sm text-gray-600">
+      <span class="font-semibold text-gray-800">${count}</span> results
+    </p>`;
+};
+
+const get = (target_url, responseFunction) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", getCookie("Authorization"));
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      const jsonData = JSON.parse(result);
+      responseFunction(jsonData);
+
+      // Hitung jumlah data dan perbarui tampilan
+      const count = jsonData.length;
+      CountSeleksi(count);
+    })
+    .catch((error) => console.log("error", error));
+};
+
+get(URLGetSeleksi, responseData);
