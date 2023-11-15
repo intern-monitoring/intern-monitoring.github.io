3cdb1662394c2e5542e04e9cf449703a8a83ff7e
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-15 20:09:40 +0700

    Add fetch function for detail seleksi mahasiswa and get function for seleksi mahasiswa

---
+++ pages/admin/assets/js/getFunction/seleksiMahasiswa/getSeleksiMhsFunction.js
@@ -0,0 +1,34 @@
+import { URLGetSeleksiMhs, responseData } from "./getSeleksiMhs.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+const CountSeleksiMhs = (count) => {
+  const resultCountElement = document.getElementById("seleksiMhsCount");
+  resultCountElement.innerHTML = `
+    <p class="text-sm text-gray-600">
+      <span class="font-semibold text-gray-800">${count}</span> results
+    </p>`;
+};
+
+const get = (target_url, responseFunction) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", getCookie("Authorization"));
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      const jsonData = JSON.parse(result);
+      responseFunction(jsonData);
+
+      // Hitung jumlah data dan perbarui tampilan
+      const count = jsonData.length;
+      CountSeleksiMhs(count);
+    })
+    .catch((error) => console.log("error", error));
+};
+
+get(URLGetSeleksiMhs, responseData);
