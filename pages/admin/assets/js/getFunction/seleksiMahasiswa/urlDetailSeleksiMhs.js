e3c90ea127246080542005b58d6b262cd4eeab94
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-16 19:49:00 +0700

    Update URL for intermoni-mahasiswa API

--- pages/admin/assets/js/getFunction/seleksiMahasiswa/urlDetailSeleksiMhs.js
+++ pages/admin/assets/js/getFunction/seleksiMahasiswa/urlDetailSeleksiMhs.js
@@ -2,5 +2,5 @@ const urlParams = new URLSearchParams(window.location.search);
 const seleksiMhsId = urlParams.get("seleksiMhsId");
 
 export const urlFetch =
-  "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-mahasiswa-magang?id=" +
+  "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-mahasiswa?id=" +
   seleksiMhsId;
