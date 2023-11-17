db661016ed64a00068248161445cbf4ac6382659
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-17 11:44:14 +0700

    Add seleksiMitra feature

---
+++ pages/admin/assets/js/getFunction/seleksiMitra/getSeleksiMitraFunction.js
@@ -0,0 +1,34 @@
+import { URLGetSeleksiMitra, responseData } from "./getSeleksiMitra.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+const CountSeleksiMitra = (count) => {
+  const resultCountElement = document.getElementById("seleksiMitraCount");
+  resultCountElement.innerHTML = `
+    <p class="text-sm text-gray-600">
+      <span class="font-semibold text-gray-800">${count}</span> results
+    </p>`;
+};
+
+const get = (target_url, responseFunction) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", getCookie("Authorization"));
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      const jsonData = JSON.parse(result);
+      responseFunction(jsonData);
+
+      // Hitung jumlah data dan perbarui tampilan
+      const count = jsonData.length;
+      CountSeleksiMitra(count);
+    })
+    .catch((error) => console.log("error", error));
+};
+
+get(URLGetSeleksiMitra, responseData);
