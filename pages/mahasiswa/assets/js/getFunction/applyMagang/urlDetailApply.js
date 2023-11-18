0ed15ef3af68386fb4aa7aea5f306773b8e75152
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-18 22:20:43 +0700

    Update URLs for applyMagang functions

--- pages/mahasiswa/assets/js/getFunction/applyMagang/urlDetailApply.js
+++ pages/mahasiswa/assets/js/getFunction/applyMagang/urlDetailApply.js
@@ -2,5 +2,5 @@ const urlParams = new URLSearchParams(window.location.search);
 const applyId = urlParams.get("applyId");
 
 export const urlFetch =
-  "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-seleksi?id=" +
+  "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-mahasiswa-magang?id=" +
   applyId;
