65648294c7e34df3d4e78b661d0e6496ea95db75
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-20 23:18:31 +0700

    Refactor dropdownMentor.js to use Authorization token from cookie

--- pages/mitra/assets/js/updateFunction/penentuanMentor/dropdownMentor.js
+++ pages/mitra/assets/js/updateFunction/penentuanMentor/dropdownMentor.js
@@ -29,38 +29,95 @@
 //   }
 // });
 
-fetch(
-  "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-magang"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const mentorDropdown = document.getElementById("namalengkapmentor");
-    console.log(data);
+// fetch(
+//   "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-magang"
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const mentorDropdown = document.getElementById("namalengkapmentor");
+//     console.log(data);
 
-    if (Array.isArray(data)) {
-      // Iterasi setiap item dalam array
-      data.forEach((item) => {
-        const option = document.createElement("option");
-        option.value = item._id;
-        option.text = item.posisi;
-        mentorDropdown.appendChild(option);
-      });
+//     if (Array.isArray(data)) {
+//       // Iterasi setiap item dalam array
+//       data.forEach((item) => {
+//         const option = document.createElement("option");
+//         option.value = item._id;
+//         option.text = item.posisi;
+//         mentorDropdown.appendChild(option);
+//       });
 
-      // Event listener untuk perubahan dropdown
-      mentorDropdown.addEventListener("change", () => {
-        const selectedId = mentorDropdown.value;
-        const selectedMentor = data.find((item) => item._id === selectedId);
+//       // Event listener untuk perubahan dropdown
+//       mentorDropdown.addEventListener("change", () => {
+//         const selectedId = mentorDropdown.value;
+//         const selectedMentor = data.find((item) => item._id === selectedId);
 
-        console.log(selectedMentor);
-        // Lakukan sesuatu dengan selectedMentor...
-        if (selectedMentor) {
-          namaMentorInput.value = selectedMentor.posisi;
-        } else {
-          namaMentorInput.value = "";
-        }
-      });
-    } else {
-      console.error("Data is not an array."); // Handle kasus di mana data bukan array
-    }
-  })
-  .catch((error) => console.log(error));
+//         console.log(selectedMentor);
+//         // Lakukan sesuatu dengan selectedMentor...
+//         if (selectedMentor) {
+//           namaMentorInput.value = selectedMentor.posisi;
+//         } else {
+//           namaMentorInput.value = "";
+//         }
+//       });
+//     } else {
+//       console.error("Data is not an array."); // Handle kasus di mana data bukan array
+//     }
+//   })
+//   .catch((error) => console.log(error));
+
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+// Dapatkan nilai Authorization dari cookie
+const authorizationToken = getCookie("Authorization");
+
+// Pastikan bahwa token Authorization ada sebelum menambahkannya ke header
+if (authorizationToken) {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", authorizationToken);
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  const apiUrl =
+    "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-magang";
+
+  fetch(apiUrl, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      const mentorDropdown = document.getElementById("namalengkapmentor");
+      console.log(data);
+
+      // Memastikan data adalah objek
+      if (typeof data === "object" && data !== null) {
+        // Iterasi setiap properti dalam objek
+        Object.entries(data).forEach(([id, item]) => {
+          const option = document.createElement("option");
+          option.value = id;
+          option.text = item.posisi;
+          mentorDropdown.appendChild(option);
+        });
+
+        // Event listener untuk perubahan dropdown
+        mentorDropdown.addEventListener("change", () => {
+          const selectedId = mentorDropdown.value;
+          const selectedMentor = data[selectedId];
+
+          // Lakukan sesuatu dengan selectedMentor...
+          console.log(selectedMentor);
+          if (selectedMentor) {
+            namaMentorInput.value = selectedMentor.posisi;
+          } else {
+            namaMentorInput.value = "";
+          }
+        });
+      } else {
+        console.error("Data is not an object."); // Handle kasus di mana data bukan objek
+      }
+    })
+    .catch((error) => console.error(error));
+} else {
+  console.error("Authorization token not found in cookies."); // Handle jika token Authorization tidak ditemukan
+}
