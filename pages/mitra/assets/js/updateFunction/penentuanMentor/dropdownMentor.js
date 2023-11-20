7f40cafcb79c7713434b8831117da06510cde162
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-20 23:02:29 +0700

    Update dropdownMentor.js and penentuanMentor.html

--- pages/mitra/assets/js/updateFunction/penentuanMentor/dropdownMentor.js
+++ pages/mitra/assets/js/updateFunction/penentuanMentor/dropdownMentor.js
@@ -1,30 +1,59 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  try {
-    // Mendapatkan elemen dropdown
-    const dropdown = document.getElementById("namalengkapmentor");
+// document.addEventListener("DOMContentLoaded", async () => {
+//   try {
+//     // Mendapatkan elemen dropdown
+//     const dropdown = document.getElementById("namalengkapmentor");
 
-    // Clear existing options
-    dropdown.innerHTML = "";
+//     // Clear existing options
+//     dropdown.innerHTML = "";
 
-    // Mengambil data dari API
-    const response = await fetch(
-      "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-magang"
-    );
+//     // Mengambil data dari API
+//     const response = await fetch(
+//       "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-magang"
+//     );
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
 
-    const data = await response.json();
+//     const data = await response.json();
 
-    // Memasukkan data dari API ke dalam dropdown
-    data.forEach((mentor) => {
+//     // Memasukkan data dari API ke dalam dropdown
+//     data.forEach((mentor) => {
+//       const option = document.createElement("option");
+//       option.value = mentor.posisi;
+//       option.text = mentor.posisi;
+//       dropdown.appendChild(option);
+//     });
+//   } catch (error) {
+//     console.error("Error fetching or parsing data:", error);
+//   }
+// });
+
+fetch(
+  "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-magang"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    const mentorDropdown = document.getElementById("namalengkapmentor");
+
+    // Generate options for each data item
+    data.forEach((item) => {
       const option = document.createElement("option");
-      option.value = mentor.posisi;
-      option.text = mentor.posisi;
-      dropdown.appendChild(option);
+      option.value = item._id;
+      option.text = item.posisi;
+      mentorDropdown.appendChild(option);
     });
-  } catch (error) {
-    console.error("Error fetching or parsing data:", error);
-  }
-});
+
+    // Event listener for polyclinic code dropdown change
+    mentorDropdown.addEventListener("change", () => {
+      const selectedId = mentorDropdown.value;
+      const selectedMentor = data.find((item) => item._id === selectedId);
+
+      if (selectedMentor) {
+        namaMentorInput.value = selectedMentor.posisi;
+      } else {
+        namaMentorInput.value = "";
+      }
+    });
+  })
+  .catch((error) => console.log(error));
