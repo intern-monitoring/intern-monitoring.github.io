aa2221fd91525f6b3f310ce44a2549ac67066f13
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-20 17:03:15 +0700

    Refactor donutCharts.js to remove unused code and update chart colors

--- pages/admin/assets/js/charts/donutCharts.js
+++ pages/admin/assets/js/charts/donutCharts.js
@@ -3,13 +3,6 @@ import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 const URLGetUser =
   "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-user";
 
-const colors = {
-  blue: "bg-blue-500",
-  teal: "bg-teal-500",
-  indigo: "bg-indigo-500",
-  gray: "bg-gray-500",
-};
-
 const get = (target_url, responseFunction) => {
   const myHeaders = new Headers();
   myHeaders.append("Authorization", getCookie("Authorization"));
@@ -43,8 +36,7 @@ const get = (target_url, responseFunction) => {
         mahasiswaCount,
         mitraCount,
         pembimbingCount,
-        mentorCount,
-        colors
+        mentorCount
       );
     })
     .catch((error) => console.log("error", error));
@@ -53,77 +45,60 @@ const get = (target_url, responseFunction) => {
 window.addEventListener("load", () => {
   get(
     URLGetUser,
-    (mahasiswaCount, mitraCount, pembimbingCount, mentorCount, colors) => {
-      buildChart(
-        "#hs-donut-chart",
-        () => ({
-          chart: {
-            height: 170,
-            width: 170,
-            type: "donut",
-            zoom: {
-              enabled: false,
-            },
-          },
-          plotOptions: {
-            pie: {
-              donut: {
-                size: "76%",
-              },
-            },
-          },
-          series: [mahasiswaCount, mitraCount, pembimbingCount, mentorCount],
-          labels: ["Mahasiswa", "Mitra", "Pembimbing", "Mentor"],
-          legend: {
-            show: false,
-          },
-          dataLabels: {
+    (mahasiswaCount, mitraCount, pembimbingCount, mentorCount) => {
+      buildChart("#hs-donut-chart", () => ({
+        chart: {
+          height: 170,
+          width: 170,
+          type: "donut",
+          zoom: {
             enabled: false,
           },
-          stroke: {
-            width: 5,
-          },
-          grid: {
-            padding: {
-              top: -12,
-              bottom: -11,
-              left: -12,
-              right: -12,
+        },
+        plotOptions: {
+          pie: {
+            donut: {
+              size: "76%",
             },
           },
-          states: {
-            hover: {
-              filter: {
-                type: "none",
-              },
+        },
+        series: [mahasiswaCount, mitraCount, pembimbingCount, mentorCount],
+        labels: ["Mahasiswa", "Mitra", "Pembimbing", "Mentor"],
+        legend: {
+          show: false,
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          width: 5,
+        },
+        grid: {
+          padding: {
+            top: -12,
+            bottom: -11,
+            left: -12,
+            right: -12,
+          },
+        },
+        states: {
+          hover: {
+            filter: {
+              type: "none",
             },
           },
-        }),
-        {
-          colors: [colors.blue],
-          stroke: {
-            colors: [colors.blue],
-          },
         },
-        {
-          colors: [colors.teal],
-          stroke: {
-            colors: [colors.teal],
-          },
-        },
-        {
-          colors: [colors.indigo],
-          stroke: {
-            colors: [colors.indigo],
-          },
-        },
-        {
-          colors: [colors.gray],
-          stroke: {
-            colors: [colors.gray],
-          },
-        }
-      );
+        colors: [
+          "#3b82f6",
+          "#22d3ee",
+          "#7dd3fc",
+          "#3b82f6",
+          "#bfdbfe",
+          "#60a5fa",
+          "#93c5fd",
+          "#1e3a8a",
+        ],
+      }));
     }
   );
 });
