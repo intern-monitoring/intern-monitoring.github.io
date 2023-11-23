a4b360ed02c1d85616e708ba3fe39888e905c0e6
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-23 18:05:10 +0700

    Add URL files for pemilihanPembimbing

---
+++ pages/admin/assets/js/getFunction/pemilihanPembimbing/getPemilihanPembimbingFunction.js
@@ -0,0 +1,41 @@
+import {
+  URLGetPemilihanPembimbing,
+  responseData,
+} from "./getPemilihanPembimbing.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+const CountPemilihanPembimbing = (count) => {
+  const resultCountElement = document.getElementById(
+    "countPemilihanPembimbing"
+  );
+  resultCountElement.innerHTML = `
+    <p class="text-sm text-gray-600">
+      <span class="font-semibold text-gray-800">${count}</span> results
+    </p>`;
+};
+
+const get = (target_url, responseFunction) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", getCookie("Authorization"));
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      const parsedResult = JSON.parse(result);
+
+      const filteredData = parsedResult.filter((user) => user.status);
+
+      // Call the response function with the filtered data
+      responseFunction(filteredData);
+
+      CountPemilihanPembimbing(filteredData.length);
+    })
+    .catch((error) => console.log("error", error));
+};
+
+get(URLGetPemilihanPembimbing, responseData);
