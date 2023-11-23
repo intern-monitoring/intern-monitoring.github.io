e3d00b6379638febfd0c1f58e9d61d8a4e71eb37
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-23 12:55:31 +0700

    Update API endpoints in getApply.js and urlDetailApply.js

--- pages/mahasiswa/assets/js/getFunction/applyMagang/getApply.js
+++ pages/mahasiswa/assets/js/getFunction/applyMagang/getApply.js
@@ -1,7 +1,7 @@
 import { addInner } from "https://jscroot.github.io/element/croot.js";
 
 export const URLGetApply =
-  "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-mahasiswa-magang";
+  "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-mahasiswa-magang/intermoni-mahasiswa-magang/seleksi";
 
 export const tableBelumKonfirmasi = `
 <tr>
