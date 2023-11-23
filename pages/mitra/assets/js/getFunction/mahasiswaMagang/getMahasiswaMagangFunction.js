e570def0ee486288be0714e1a6d5cec5a4de092c
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-23 23:25:36 +0700

    Refactor getMahasiswaMagangFunction.js

--- pages/mitra/assets/js/getFunction/mahasiswaMagang/getMahasiswaMagangFunction.js
+++ pages/mitra/assets/js/getFunction/mahasiswaMagang/getMahasiswaMagangFunction.js
@@ -1,143 +1,141 @@
 import { URLGetMahasiswaMagang, responseData } from "./getMahasiswaMagang.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-// const CountMahasiswaMagang = (count) => {
-//   const resultCountElement = document.getElementById("countMahasiswaMagang");
-//   resultCountElement.innerHTML = `
-//     <p class="text-sm text-gray-600">
-//       <span class="font-semibold text-gray-800">${count}</span> results
-//     </p>`;
-// };
+const CountMahasiswaMagang = (count) => {
+  const resultCountElement = document.getElementById("countMahasiswaMagang");
+  resultCountElement.innerHTML = `
+    <p class="text-sm text-gray-600">
+      <span class="font-semibold text-gray-800">${count}</span> results
+    </p>`;
+};
 
-// const get = (target_url, responseFunction) => {
-//   const myHeaders = new Headers();
-//   myHeaders.append("Authorization", getCookie("Authorization"));
-//   const requestOptions = {
-//     method: "GET",
-//     headers: myHeaders,
-//     redirect: "follow",
-//   };
+const get = (target_url, responseFunction) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", getCookie("Authorization"));
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
 
-//   fetch(target_url, requestOptions)
-//     .then((response) => response.text())
-//     .then((result) => {
-//       const parsedResult = JSON.parse(result);
+  fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      const parsedResult = JSON.parse(result);
 
-//       // Store the initial data
-//       const initialData = parsedResult;
+      // Store the initial data in local storage
+      localStorage.setItem("initialData", JSON.stringify(parsedResult));
 
-//       // Listen for changes to the search input value
-//       const searchInput = document.getElementById("search-mahasiswa-magang");
-//       searchInput.addEventListener("keyup", () => {
-//         // Filter the data based on the current search query
-//         const searchQuery = searchInput.value.toLowerCase();
-//         let filteredData;
+      // Listen for changes to the search input value
+      const searchInput = document.getElementById("search-mahasiswa-magang");
+      searchInput.addEventListener("keyup", () => {
+        // Filter the data based on the current search query
+        const searchQuery = searchInput.value.toLowerCase();
+        let filteredData;
 
-//         if (searchQuery) {
-//           filteredData = parsedResult.filter((user) => {
-//             return user.mahasiswa.namalengkap
-//               .toLowerCase()
-//               .includes(searchQuery);
-//           });
-//         } else {
-//           // Use the initial data
-//           filteredData = initialData;
-//         }
+        if (searchQuery) {
+          filteredData = parsedResult.filter((user) => {
+            return user.mahasiswa.namalengkap
+              .toLowerCase()
+              .includes(searchQuery);
+          });
+        } else {
+          // Use the initial data from local storage
+          filteredData = localStorage.getItem("initialData")
+            ? JSON.parse(localStorage.getItem("initialData"))
+            : [];
+        }
 
-//         // Update the result count and call the response function with the filtered data
-//         CountMahasiswaMagang(filteredData.length);
-//         responseFunction(filteredData);
-
-//         // Print the search query to the console log
-//         console.log(searchInput.value);
-//       });
-//     })
-//     .catch((error) => console.log("error", error));
-// };
-
-// get(URLGetMahasiswaMagang, responseData);
+        // Update the result count and call the response function with the filtered data
+        CountMahasiswaMagang(filteredData.length);
+        responseFunction(filteredData);
+      });
+    })
+    .catch((error) => console.log("error", error));
+};
+get(URLGetMahasiswaMagang, responseData);
 
 // Test
-const fetchData = async () => {
-  try {
-    // Hapus data lama dari localStorage sebelum mendapatkan data baru
-    // localStorage.removeItem("magangData");
+// const fetchData = async () => {
+//   try {
+//     // Hapus data lama dari localStorage sebelum mendapatkan data baru
+//     // localStorage.removeItem("magangData");
 
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", getCookie("Authorization"));
-    const requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
-    };
+//     const myHeaders = new Headers();
+//     myHeaders.append("Authorization", getCookie("Authorization"));
+//     const requestOptions = {
+//       method: "GET",
+//       headers: myHeaders,
+//       redirect: "follow",
+//     };
 
-    const response = await fetch(URLGetMahasiswaMagang, requestOptions);
-    const data = await response.json();
+//     const response = await fetch(URLGetMahasiswaMagang, requestOptions);
+//     const data = await response.json();
 
-    // Simpan data baru ke localStorage
-    localStorage.setItem("mahasiswaMagang", JSON.stringify(data));
+//     // Simpan data baru ke localStorage
+//     localStorage.setItem("mahasiswaMagang", JSON.stringify(data));
 
-    responseData(data);
-  } catch (error) {
-    console.error("Error fetching or processing data: ", error);
-  }
-};
+//     responseData(data);
+//   } catch (error) {
+//     console.error("Error fetching or processing data: ", error);
+//   }
+// };
 
-window.onload = function () {
-  fetchData(); // Fungsi untuk menampilkan semua data saat halaman dimuat
-  const searchInput = document.getElementById("search-mahasiswa-magang");
+// window.onload = function () {
+//   fetchData(); // Fungsi untuk menampilkan semua data saat halaman dimuat
+//   const searchInput = document.getElementById("search-mahasiswa-magang");
 
-  searchInput.addEventListener("input", function () {
-    searchData(this.value);
-  });
-};
+//   searchInput.addEventListener("input", function () {
+//     searchData(this.value);
+//   });
+// };
 
-const searchData = async () => {
-  const namaInput = document
-    .getElementById("search-mahasiswa-magang")
-    .value.toLowerCase();
+// const searchData = async () => {
+//   const namaInput = document
+//     .getElementById("search-mahasiswa-magang")
+//     .value.toLowerCase();
 
-  try {
-    let data = localStorage.getItem("mahasiswaMagang");
+//   try {
+//     let data = localStorage.getItem("mahasiswaMagang");
 
-    if (!data) {
-      const myHeaders = new Headers();
-      myHeaders.append("Authorization", getCookie("Authorization"));
-      const requestOptions = {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "follow",
-      };
+//     if (!data) {
+//       const myHeaders = new Headers();
+//       myHeaders.append("Authorization", getCookie("Authorization"));
+//       const requestOptions = {
+//         method: "GET",
+//         headers: myHeaders,
+//         redirect: "follow",
+//       };
 
-      const response = await fetch(URLGetMahasiswaMagang, requestOptions);
-      data = await response.json();
-      localStorage.setItem("mahasiswaMagang", JSON.stringify(data));
-    } else {
-      data = JSON.parse(data);
-    }
+//       const response = await fetch(URLGetMahasiswaMagang, requestOptions);
+//       data = await response.json();
+//       localStorage.setItem("mahasiswaMagang", JSON.stringify(data));
+//     } else {
+//       data = JSON.parse(data);
+//     }
 
-    if (Array.isArray(data)) {
-      const filteredResults = data.filter((item) => {
-        const nama = (item.mahasiswa.namalengkap || "").toLowerCase(); // Periksa apakah posisi ada
+//     if (Array.isArray(data)) {
+//       const filteredResults = data.filter((item) => {
+//         const nama = (item.mahasiswa.namalengkap || "").toLowerCase(); // Periksa apakah posisi ada
 
-        return nama.includes(namaInput);
-      });
+//         return nama.includes(namaInput);
+//       });
 
-      const mhsMagangContainer = document.getElementById(
-        "tableMahasiswaMagang"
-      );
-      mhsMagangContainer.innerHTML = "";
-      if (namaInput === "") {
-        // Jika semua input kosong, tampilkan semua data
-        responseData(data);
-      } else {
-        // Jika ada kriteria pencarian, tampilkan hasil pencarian
-        responseData(filteredResults);
-      }
-    } else {
-      console.error("Data is not an array:", data);
-    }
-  } catch (error) {
-    console.error("Error searching data: ", error);
-  }
-};
+//       const mhsMagangContainer = document.getElementById(
+//         "tableMahasiswaMagang"
+//       );
+//       mhsMagangContainer.innerHTML = "";
+//       if (namaInput === "") {
+//         // Jika semua input kosong, tampilkan semua data
+//         responseData(data);
+//       } else {
+//         // Jika ada kriteria pencarian, tampilkan hasil pencarian
+//         responseData(filteredResults);
+//       }
+//     } else {
+//       console.error("Data is not an array:", data);
+//     }
+//   } catch (error) {
+//     console.error("Error searching data: ", error);
+//   }
+// };
