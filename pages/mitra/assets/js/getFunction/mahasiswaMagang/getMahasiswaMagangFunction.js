d0037588d824497902d905fccd5e770fed7cba44
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-23 23:46:08 +0700

    Remove commented out code in getMahasiswaMagangFunction.js

--- pages/mitra/assets/js/getFunction/mahasiswaMagang/getMahasiswaMagangFunction.js
+++ pages/mitra/assets/js/getFunction/mahasiswaMagang/getMahasiswaMagangFunction.js
@@ -1,65 +1,6 @@
 import { URLGetMahasiswaMagang, responseData } from "./getMahasiswaMagang.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-// const CountMahasiswaMagang = (count) => {
-//   const resultCountElement = document.getElementById("countMahasiswaMagang");
-//   resultCountElement.innerHTML = ""; // Clear the existing data
-
-//   resultCountElement.innerHTML = `
-//     <p class="text-sm text-gray-600">
-//       <span class="font-semibold text-gray-800">${count}</span> results
-//     </p>`;
-// };
-
-// const get = (target_url, responseFunction) => {
-//   const myHeaders = new Headers();
-//   myHeaders.append("Authorization", getCookie("Authorization"));
-//   const requestOptions = {
-//     method: "GET",
-//     headers: myHeaders,
-//     redirect: "follow",
-//   };
-
-//   fetch(target_url, requestOptions)
-//     .then((response) => response.text())
-//     .then((result) => {
-//       const parsedResult = JSON.parse(result);
-
-//       // Store the initial data
-//       const initialData = parsedResult;
-
-//       // Listen for changes to the search input value
-//       const searchInput = document.getElementById("search-mahasiswa-magang");
-//       searchInput.addEventListener("keyup", () => {
-//         // Filter the data based on the current search query
-//         const searchQuery = searchInput.value.toLowerCase();
-//         let filteredData;
-
-//         if (searchQuery) {
-//           filteredData = parsedResult.filter((user) => {
-//             return user.mahasiswa.namalengkap
-//               .toLowerCase()
-//               .includes(searchQuery);
-//           });
-//         } else {
-//           // Use the initial data
-//           filteredData = initialData;
-//         }
-
-//         // Clear the existing data before adding the filtered data
-//         CountMahasiswaMagang(0); // Reset the count
-
-//         // Update the result count and call the response function with the filtered data
-//         CountMahasiswaMagang(filteredData.length);
-//         responseFunction(filteredData);
-//       });
-//     })
-//     .catch((error) => console.log("error", error));
-// };
-
-// get(URLGetMahasiswaMagang, responseData);
-
-// Test
 const fetchData = async () => {
   try {
     const myHeaders = new Headers();
