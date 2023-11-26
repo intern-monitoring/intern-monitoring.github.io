7036d6052219bd48272abfc0d3e86be1b9a6140a
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-26 20:59:05 +0700

    Refactor daily report layout

--- pages/mahasiswa/assets/js/getFunction/dailyReport/detailReport.js
+++ pages/mahasiswa/assets/js/getFunction/dailyReport/detailReport.js
@@ -3,65 +3,110 @@ import { convertToWIB } from "./convertToWib.js";
 
 export const dataDailyReport = `
 <div class="bg-white rounded-xl border shadow-md p-4 sm:p-7">
-<div>
-  <div>
-    <h2 class="text-xl font-semibold text-gray-800">Data report</h2>
-    <p class="text-sm text-gray-600">
-      Berikut merupakan data daily report magang.
-    </p>
-  </div>
-  <div class="border-b pt-1 border-gray-200"></div>
-  <div class="pt-5">
-    <div>
-      <h3 class="text-md font-semibold text-gray-800">
-        Judul daily report :
-      </h3>
-      <p class="text-gray-800 text-sm">#JUDUL#</p>
-    </div>
+            <div class="grid gap-4">
+              <div>
+                <h2 class="text-2xl font-semibold text-gray-800">
+                  Data report
+                </h2>
+                <p class="text-md text-gray-600 pb-1">
+                  Berikut merupakan data daily report magang.
+                </p>
+                <div class="border-b pt-1 border-gray-200"></div>
+              </div>
+              <div>
+                <div class="grid space-y-2">
+                  <!-- Pembimbing -->
+                  <div class="pb-4">
+                    <h2 class="text-lg font-semibold text-gray-800">
+                      Penerima Report :
+                    </h2>
+                  </div>
+                  <div class="flex gap-x-8 text-sm items-center">
+                    <div class="pb-3">
+                      <img
+                        class="inline-block h-28 w-28 rounded-full ring-2 ring-white"
+                        src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+                        alt="Image Description"
+                      />
+                    </div>
+                    <div class="space-y-1">
+                      <div>
+                        <h3 class="font-semibold text-gray-800">
+                          Nama Pembimbing :
+                        </h3>
+                        <p class="text-gray-500">#PEMBIMBING#</p>
+                      </div>
+                      <div>
+                        <span class="block font-semibold">NIK :</span>
+                        <p class="text-gray-500">#NIKPEMBIMBING#</p>
+                      </div>
+                      <div>
+                        <span class="block font-semibold">Dosen Prodi :</span>
+                        <p class="text-gray-500">#PRODI#</p>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- End Pembimbing -->
+                </div>
+              </div>
+              <!-- Col -->
+              <div>
+                <div>
+                  <div>
+                    <h2 class="text-lg font-semibold text-gray-800">
+                      Isi Report :
+                    </h2>
+                    <div class="border-b pt-1 border-gray-200"></div>
+                  </div>
+                  <div class="py-4">
+                    <h3 class="text-md font-semibold text-gray-800">
+                      Judul daily report :
+                    </h3>
+                    <p class="text-gray-800 text-sm">#JUDUL#</p>
+                  </div>
+                  <div class="">
+                    <h3 class="text-md font-semibold text-gray-800">
+                      Deskripsi daily report :
+                    </h3>
 
-    <div class="pt-3">
-      <h3 class="text-md font-semibold text-gray-800">
-        Deskripsi daily report :
-      </h3>
-
-      <p class="text-gray-800 text-sm">#DESKRIPSI#</p>
-      <div class="border-b border-gray-200 pt-5"></div>
-    </div>
-    <div class="pt-4 flex justify-between items-start">
-      <div class="flex items-center gap-x-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-clock"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
-          />
-          <path
-            d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"
-          />
-        </svg>
-        <p class="text-md font-semibold text-gray-800">
-          Dibuat tanggal :
-          <span class="text-gray-800 text-sm font-normal"
-            >#TANGGAL#</span
-          >
-        </p>
-      </div>
-      <a
-        href="dailyReport.html"
-        class="py-2 px-3 inline-flex cursor-pointer justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm"
-      >
-        Kembali
-      </a>
-    </div>
-  </div>
-</div>
-<div class="mt-2 flex justify-end gap-x-2"></div>
-</div>
+                    <p class="text-gray-800 text-sm">#DESKRIPSI#</p>
+                    <div class="border-b border-gray-200 pt-5"></div>
+                  </div>
+                  <div class="pt-4 flex justify-between items-start">
+                    <div class="flex items-center gap-x-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-clock"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
+                        />
+                        <path
+                          d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"
+                        />
+                      </svg>
+                      <p class="text-md font-semibold text-gray-800">
+                        Terikirim pada :
+                        <span class="text-gray-800 text-sm font-normal"
+                          >#TANGGAL#</span
+                        >
+                      </p>
+                    </div>
+                    <a
+                      href="dailyReport.html"
+                      class="py-2 px-3 inline-flex cursor-pointer justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm"
+                    >
+                      Kembali
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 `;
 
 export function responseData(results) {
@@ -72,6 +117,9 @@ export function responseData(results) {
 export function isiRow(value) {
   const wibCreated = convertToWIB(value.createdat);
   const content = dataDailyReport
+    .replace("#PEMBIMBING#", value.penerima.nama)
+    .replace("#NIKPEMBIMBING#", value.penerima.nik)
+    .replace("#PRODI#", value.penerima.prodi)
     .replace("#JUDUL#", value.judul)
     .replace("#DESKRIPSI#", value.isi)
     .replace("#TANGGAL#", wibCreated);
