de17713a4d58ea78b4f01ac201bf416e122dafce
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-27 17:05:19 +0700

    Fix async/await issue in get function

--- pages/mahasiswa/assets/js/getFunction/mahasiswaMagang/getMhsMagangFunction.js
+++ pages/mahasiswa/assets/js/getFunction/mahasiswaMagang/getMhsMagangFunction.js
@@ -1,7 +1,7 @@
 import { URLGetMahasiswaMagang, responseData } from "./getMhsMagang.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-export const get = async (target_url, responseFunction) => {
+export const get = (target_url, responseFunction) => {
   const myHeaders = new Headers();
   myHeaders.append("Authorization", getCookie("Authorization"));
   const requestOptions = {
@@ -10,13 +10,15 @@ export const get = async (target_url, responseFunction) => {
     redirect: "follow",
   };
 
-  await fetch(target_url, requestOptions)
+  fetch(target_url, requestOptions)
     .then((response) => response.text())
     .then((result) => responseFunction(JSON.parse(result)))
     .catch((error) => console.log("error", error));
 };
 
 get(URLGetMahasiswaMagang, responseData, responseID);
+console.log(pembimbingID);
+console.log(mentorID);
 
 export let pembimbingID;
 export let mentorID;
