bdd397279efaf4cfad0b494da23fa4913aaccbb1
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-27 17:03:00 +0700

    Add async keyword to get function in getMhsMagangFunction.js

--- pages/mahasiswa/assets/js/getFunction/mahasiswaMagang/getMhsMagangFunction.js
+++ pages/mahasiswa/assets/js/getFunction/mahasiswaMagang/getMhsMagangFunction.js
@@ -1,7 +1,7 @@
 import { URLGetMahasiswaMagang, responseData } from "./getMhsMagang.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-export const get = (target_url, responseFunction) => {
+export const get = async (target_url, responseFunction) => {
   const myHeaders = new Headers();
   myHeaders.append("Authorization", getCookie("Authorization"));
   const requestOptions = {
@@ -10,19 +10,20 @@ export const get = (target_url, responseFunction) => {
     redirect: "follow",
   };
 
-  fetch(target_url, requestOptions)
+  await fetch(target_url, requestOptions)
     .then((response) => response.text())
     .then((result) => responseFunction(JSON.parse(result)))
     .catch((error) => console.log("error", error));
 };
+
 get(URLGetMahasiswaMagang, responseData, responseID);
 
 export let pembimbingID;
 export let mentorID;
 
-async function responseID(results) {
+function responseID(results) {
   console.log(results);
-  await results.forEach((result) => {
+  results.forEach((result) => {
     getIDMentor(result);
     getIDPembimbing(result);
   });
