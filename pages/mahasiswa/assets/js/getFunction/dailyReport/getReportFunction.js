01baebbf583df5a1e5b7974ceeb9f2f1bce3d696
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-27 16:27:49 +0700

    Refactor fetch functions to use async/await

--- pages/mahasiswa/assets/js/getFunction/dailyReport/getReportFunction.js
+++ pages/mahasiswa/assets/js/getFunction/dailyReport/getReportFunction.js
@@ -1,12 +1,7 @@
-import {
-  URLGetReport,
-  responseData,
-  // pembimbingID,
-  // mentorID,
-} from "./getReport.js";
+import { URLGetReport, responseData } from "./getReport.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-const get = (target_url, responseFunction) => {
+const get = async (target_url, responseFunction) => {
   const myHeaders = new Headers();
   myHeaders.append("Authorization", getCookie("Authorization"));
   const requestOptions = {
@@ -15,24 +10,14 @@ const get = (target_url, responseFunction) => {
     redirect: "follow",
   };
 
-  fetch(target_url, requestOptions)
-    .then((response) => response.text())
-    .then((result) => {
-      const jsonData = JSON.parse(result);
-      responseFunction(jsonData);
-
-      // Hitung jumlah data dan perbarui tampilan
-      // const countPembimbing = jsonData.filter(
-      //   (value) => pembimbingID === value.penerima._id
-      // ).length;
-      // const countMentor = jsonData.filter(
-      //   (value) => mentorID === value.penerima._id
-      // ).length;
-
-      // CountReportPembimbing(countPembimbing);
-      // CountReportMentor(countMentor);
-    })
-    .catch((error) => console.log("error", error));
+  try {
+    const response = await fetch(target_url, requestOptions);
+    const result = await response.text();
+    const jsonData = JSON.parse(result);
+    responseFunction(jsonData);
+  } catch (error) {
+    console.log("error", error);
+  }
 };
 
 get(URLGetReport, responseData);
--- pages/mahasiswa/assets/js/getFunction/mahasiswaMagang/getMhsMagangFunction.js
+++ pages/mahasiswa/assets/js/getFunction/mahasiswaMagang/getMhsMagangFunction.js
@@ -1,7 +1,7 @@
 import { URLGetMahasiswaMagang, responseData } from "./getMhsMagang.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-export const get = (target_url, responseFunction) => {
+export const get = async (target_url, responseFunction) => {
   const myHeaders = new Headers();
   myHeaders.append("Authorization", getCookie("Authorization"));
   const requestOptions = {
@@ -10,10 +10,13 @@ export const get = (target_url, responseFunction) => {
     redirect: "follow",
   };
 
-  fetch(target_url, requestOptions)
-    .then((response) => response.text())
-    .then((result) => responseFunction(JSON.parse(result)))
-    .catch((error) => console.log("error", error));
+  try {
+    const response = await fetch(target_url, requestOptions);
+    const result = await response.text();
+    responseFunction(JSON.parse(result));
+  } catch (error) {
+    console.log("error", error);
+  }
 };
 
 get(URLGetMahasiswaMagang, responseData);
