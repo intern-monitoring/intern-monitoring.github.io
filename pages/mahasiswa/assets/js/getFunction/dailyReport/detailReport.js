47cd8238a054e06841a53fd46c5dc15a25ce3b3c
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-27 13:54:46 +0700

    Update image URLs and add mahasiswa information to detail report

--- pages/mahasiswa/assets/js/getFunction/dailyReport/detailReport.js
+++ pages/mahasiswa/assets/js/getFunction/dailyReport/detailReport.js
@@ -132,7 +132,7 @@ export const dataDailyReportMentor = `
         <div>
           <img
             class="inline-block h-28 w-28 rounded-full ring-2 ring-white"
-            src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+            src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"
             alt="Image Description"
           />
         </div>
