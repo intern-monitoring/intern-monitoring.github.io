717e7b2ba195e130c798d862015f0b9a3d462e09
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-28 18:59:38 +0700

    Add hide function to getSeleksiWawancara.js and getSeleksiWawancaraFunction.js

--- pages/mitra/assets/js/getFunction/seleksiWawancara/getSeleksiWawancaraFunction.js
+++ pages/mitra/assets/js/getFunction/seleksiWawancara/getSeleksiWawancaraFunction.js
@@ -1,5 +1,6 @@
 import { URLGetSeleksiWawancara, responseData } from "./getSeleksiWawancara.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+import { hide } from "https://jscroot.github.io/element/croot.js";
 
 const CountWawancara = (count) => {
   const resultCountElement = document.getElementById("seleksiWawancaraCount");
@@ -10,6 +11,8 @@ const CountWawancara = (count) => {
 };
 
 const get = (target_url, responseFunction) => {
+  document.getElementById("skeletonLoader").style.display = "grow";
+
   const myHeaders = new Headers();
   myHeaders.append("Authorization", getCookie("Authorization"));
   const requestOptions = {
@@ -28,7 +31,11 @@ const get = (target_url, responseFunction) => {
       const count = jsonData.length;
       CountWawancara(count);
     })
-    .catch((error) => console.log("error", error));
+    .catch((error) => {
+      console.log("error", error);
+      // Hide the skeleton loader in case of an error
+      hide("skeletonLoader");
+    });
 };
 
 get(URLGetSeleksiWawancara, responseData);
