00c6a4cdde2a407844b0f61d7a97844bd1a2ff71
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-28 18:00:10 +0700

    Add hide function to getSeleksiMitra.js and getSeleksiMitraFunction.js

--- pages/admin/assets/js/getFunction/seleksiMitra/getSeleksiMitra.js
+++ pages/admin/assets/js/getFunction/seleksiMitra/getSeleksiMitra.js
@@ -1,4 +1,4 @@
-import { addInner } from "https://jscroot.github.io/element/croot.js";
+import { addInner, hide } from "https://jscroot.github.io/element/croot.js";
 
 export const URLGetSeleksiMitra =
   "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-mitra";
@@ -282,6 +282,7 @@ export function responseData(results) {
     isiRowLolos(result);
     isiRowTidakLolos(result);
   });
+  hide("skeletonLoader");
 }
 
 export function isiRowPending(value) {
--- pages/admin/assets/js/getFunction/seleksiMitra/getSeleksiMitraFunction.js
+++ pages/admin/assets/js/getFunction/seleksiMitra/getSeleksiMitraFunction.js
@@ -1,5 +1,6 @@
 import { URLGetSeleksiMitra, responseData } from "./getSeleksiMitra.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+import { hide } from "https://jscroot.github.io/element/croot.js";
 
 const CountSeleksiMitra = (count) => {
   const resultCountElement = document.getElementById("seleksiMitraCount");
@@ -10,6 +11,8 @@ const CountSeleksiMitra = (count) => {
 };
 
 const get = (target_url, responseFunction) => {
+  document.getElementById("skeletonLoader").style.display = "grow";
+
   const myHeaders = new Headers();
   myHeaders.append("Authorization", getCookie("Authorization"));
   const requestOptions = {
@@ -28,7 +31,11 @@ const get = (target_url, responseFunction) => {
       const count = jsonData.length;
       CountSeleksiMitra(count);
     })
-    .catch((error) => console.log("error", error));
+    .catch((error) => {
+      console.log("error", error);
+      // Hide the skeleton loader in case of an error
+      hide("skeletonLoader");
+    });
 };
 
 get(URLGetSeleksiMitra, responseData);
