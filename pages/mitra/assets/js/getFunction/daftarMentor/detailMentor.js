b3043219297807803b3fc593be92b51203e554b3
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-29 12:45:34 +0700

    Add hide function to detailMentor.js and show skeletonLoader in fetchDetailMentor.js

--- pages/mitra/assets/js/getFunction/daftarMentor/detailMentor.js
+++ pages/mitra/assets/js/getFunction/daftarMentor/detailMentor.js
@@ -1,4 +1,4 @@
-import { addInner } from "https://jscroot.github.io/element/croot.js";
+import { addInner, hide } from "https://jscroot.github.io/element/croot.js";
 
 export const dataMentor = `
 <div class="bg-white rounded-xl shadow-md">
@@ -57,6 +57,7 @@ export const dataMentor = `
 export function responseData(results) {
   console.log(results);
   isiRow(results);
+  hide("skeletonLoader");
 }
 
 export function isiRow(value) {
