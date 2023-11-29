a54b42c0f8761541e53c814d30333b7f27dffca4
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-29 19:12:50 +0700

    Refactor magangCount function to filter jsonData by status

--- pages/mitra/assets/js/stats/getMagangCount.js
+++ pages/mitra/assets/js/stats/getMagangCount.js
@@ -3,7 +3,7 @@ import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 const URLGetMagang =
   "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-magang";
 
-const mahasiswaMagangCount = (count) => {
+const magangCount = (count) => {
   const resultCountElement = document.getElementById("magangCount");
   resultCountElement.innerHTML = `<h3 class="mt-1 text-xl font-medium text-gray-800">${count}</h3>`;
 };
@@ -22,9 +22,9 @@ const getMagang = (target_url) => {
     .then((result) => {
       const jsonData = JSON.parse(result);
 
-      const magangCount = jsonData.length;
+      const magangcount = jsonData.filter((item) => item.status === 1).length;
 
-      magangCount(magangCount);
+      magangCount(magangcount);
     })
     .catch((error) => console.log("error", error));
 };
