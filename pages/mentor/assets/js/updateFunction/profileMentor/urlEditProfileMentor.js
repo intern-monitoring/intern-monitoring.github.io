d1e67127d187223593dfdacc39da8d39f7db9b04
dimasardnt6 <dimasardnt6@gmail.com>
2023-12-03 19:50:16 +0700

    Delete unnecessary files

--- pages/mentor/assets/js/updateFunction/profileMentor/urlEditProfileMentor.js
+++
@@ -1,6 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const mentorId = urlParams.get("mentorId");
-
-export const urlFetch =
-  "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-mentor?id=" +
-  mentorId;
